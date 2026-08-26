2d6a6757ad17e2c8ef4a51bd469fad89c4dddf8c
miyabi-no1-fan <222554062+miyabi-no1-fan@users.noreply.github.com>
2026-08-27 05:02:49 +0700

    add a control interface for `Decoder` (similar design to audio) add Task::Seek(i64) to handle the seek

--- src/decode.rs
+++ src/decode.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+use std::sync::Mutex;
 use std::thread;
 use std::time::Duration;
 
@@ -15,6 +17,26 @@ use crate::Error;
 use crate::FPS_LIMIT;
 use crate::HEIGHT_LIMIT;
 use crate::WIDTH_LIMIT;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Status {
+    /// Decoder is still running
+    Running,
+    /// Decoder has finish, no more decode frames.
+    Finish,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Task {
+    Play,
+    Seek(i64),
+}
+
+#[derive(Debug)]
+pub struct Control {
+    pub task: Mutex<Task>,
+    pub status: Mutex<Status>,
+}
 
 pub struct Decoder {
     ictx: format::context::Input,
@@ -124,123 +146,185 @@ impl Decoder {
         audio_config: Option<cpal::SupportedStreamConfig>,
         mut video_prod: Option<Sender<frame::Video>>,
         mut audio_prod: Option<Sender<frame::Audio>>,
-    ) -> thread::JoinHandle<Result<(), Error>> {
-        thread::spawn(move || -> Result<(), Error> {
-            let mut is_video = decoder.video_decoder.is_some()
-                && decoder.video_strm_index.is_some()
-                && video_prod.is_some();
+    ) -> (thread::JoinHandle<Result<(), Error>>, Arc<Control>) {
+        let control = Arc::new(Control {
+            task: Mutex::new(Task::Play),
+            status: Mutex::new(Status::Running),
+        });
 
-            let mut is_audio = decoder.audio_decoder.is_some()
-                && decoder.audio_strm_index.is_some()
-                && audio_config.is_some()
-                && audio_prod.is_some();
+        (
+            {
+                let control = control.clone();
+                thread::spawn(move || -> Result<(), Error> {
+                    let mut is_video = decoder.video_decoder.is_some()
+                        && decoder.video_strm_index.is_some()
+                        && video_prod.is_some();
 
-            let mut scaler = if is_video {
-                Some(ffmpeg::software::scaling::Context::get(
-                    decoder.video_decoder.as_ref().unwrap().format(),
-                    decoder.video_decoder.as_ref().unwrap().width(),
-                    decoder.video_decoder.as_ref().unwrap().height(),
-                    if is_little_endian() {
-                        format::Pixel::BGRZ
+                    let mut is_audio = decoder.audio_decoder.is_some()
+                        && decoder.audio_strm_index.is_some()
+                        && audio_config.is_some()
+                        && audio_prod.is_some();
+
+                    let mut scaler = if is_video {
+                        Some(ffmpeg::software::scaling::Context::get(
+                            decoder.video_decoder.as_ref().unwrap().format(),
+                            decoder.video_decoder.as_ref().unwrap().width(),
+                            decoder.video_decoder.as_ref().unwrap().height(),
+                            if is_little_endian() {
+                                format::Pixel::BGRZ
+                            } else {
+                                format::Pixel::ZRGB
+                            }, // somehow, ZRGB32 does not work
+                            decoder.video_decoder.as_ref().unwrap().width(),
+                            decoder.video_decoder.as_ref().unwrap().height(),
+                            ffmpeg::software::scaling::flag::Flags::BILINEAR,
+                        )?)
                     } else {
-                        format::Pixel::ZRGB
-                    }, // somehow, ZRGB32 does not work
-                    decoder.video_decoder.as_ref().unwrap().width(),
-                    decoder.video_decoder.as_ref().unwrap().height(),
-                    ffmpeg::software::scaling::flag::Flags::BILINEAR,
-                )?)
-            } else {
-                None
-            };
-
-            let mut resampler = if is_audio {
-                Some(ffmpeg::software::resampling::Context::get(
-                    decoder.audio_decoder.as_ref().unwrap().format(),
-                    decoder.audio_decoder.as_ref().unwrap().channel_layout(),
-                    decoder.audio_decoder.as_ref().unwrap().rate(),
-                    audio_config.unwrap().sample_format().as_ffmpeg_sample(),
-                    ChannelLayout::default(audio_config.unwrap().channels().into()),
-                    audio_config.unwrap().sample_rate(),
-                )?)
-            } else {
-                None
-            };
-
-            for (stream, packet) in decoder.ictx.packets() {
-                // VIDEO
-                if is_video && stream.index() == decoder.video_strm_index.unwrap() {
-                    decoder
-                        .video_decoder
-                        .as_mut()
-                        .unwrap()
-                        .send_packet(&packet)?;
-
-                    is_video = Self::video_decode(
-                        decoder.video_decoder.as_mut().unwrap(),
-                        scaler.as_mut().unwrap(),
-                        &mut video_prod.as_mut().unwrap(),
-                    )?;
-                }
-                // AUDIO
-                else if is_audio && stream.index() == decoder.audio_strm_index.unwrap() {
-                    decoder
-                        .audio_decoder
-                        .as_mut()
-                        .unwrap()
-                        .send_packet(&packet)?;
-
-                    is_audio = Self::audio_decode(
-                        decoder.audio_decoder.as_mut().unwrap(),
-                        resampler.as_mut().unwrap(),
-                        audio_prod.as_mut().unwrap(),
-                    )?;
-                }
-
-                if !is_video && !is_audio {
-                    break;
-                }
-
-                loop {
-                    let v = if is_video {
-                        video_prod.as_ref().unwrap().len()
-                    } else {
-                        usize::MAX
+                        None
                     };
 
-                    let a = if is_audio {
-                        audio_prod.as_ref().unwrap().len()
+                    let mut resampler = if is_audio {
+                        Some(ffmpeg::software::resampling::Context::get(
+                            decoder.audio_decoder.as_ref().unwrap().format(),
+                            decoder.audio_decoder.as_ref().unwrap().channel_layout(),
+                            decoder.audio_decoder.as_ref().unwrap().rate(),
+                            audio_config.unwrap().sample_format().as_ffmpeg_sample(),
+                            ChannelLayout::default(audio_config.unwrap().channels().into()),
+                            audio_config.unwrap().sample_rate(),
+                        )?)
                     } else {
-                        usize::MAX
+                        None
                     };
 
-                    if v <= DECODE_QUEUE_LEN || a <= DECODE_QUEUE_LEN {
-                        break;
+                    loop {
+                        if matches!(*control.status.lock().unwrap(), Status::Finish) {
+                            break;
+                        }
+
+                        let mut task = control.task.lock().unwrap();
+                        match *task {
+                            Task::Play => drop(task),
+                            Task::Seek(seconds) => {
+                                let position = ffmpeg::Rescale::rescale(
+                                    &seconds,
+                                    (1, 1),
+                                    ffmpeg::rescale::TIME_BASE,
+                                );
+                                decoder.ictx.seek(position, ..position)?;
+
+                                if is_video {
+                                    decoder.video_decoder.as_mut().unwrap().flush();
+                                }
+
+                                if is_audio {
+                                    decoder.audio_decoder.as_mut().unwrap().flush();
+                                    Self::audio_flush(
+                                        resampler.as_mut().unwrap(),
+                                        audio_prod.as_mut().unwrap(),
+                                    )?;
+                                }
+
+                                *task = Task::Play;
+                                drop(task);
+
+                                while (is_audio && !audio_prod.as_ref().unwrap().is_empty())
+                                    || (is_video && !video_prod.as_ref().unwrap().is_empty())
+                                {
+                                    thread::sleep(Duration::from_micros(10));
+                                }
+                            }
+                        }
+
+                        let Some((stream, packet)) = decoder.ictx.packets().next() else {
+                            break;
+                        };
+
+                        // VIDEO
+                        if is_video && stream.index() == decoder.video_strm_index.unwrap() {
+                            decoder
+                                .video_decoder
+                                .as_mut()
+                                .unwrap()
+                                .send_packet(&packet)?;
+
+                            is_video = Self::video_decode(
+                                decoder.video_decoder.as_mut().unwrap(),
+                                scaler.as_mut().unwrap(),
+                                &mut video_prod.as_mut().unwrap(),
+                            )?;
+                        }
+                        // AUDIO
+                        else if is_audio && stream.index() == decoder.audio_strm_index.unwrap() {
+                            decoder
+                                .audio_decoder
+                                .as_mut()
+                                .unwrap()
+                                .send_packet(&packet)?;
+
+                            is_audio = Self::audio_decode(
+                                decoder.audio_decoder.as_mut().unwrap(),
+                                resampler.as_mut().unwrap(),
+                                audio_prod.as_mut().unwrap(),
+                            )?;
+                        }
+
+                        if !is_video && !is_audio {
+                            break;
+                        }
+
+                        loop {
+                            let v = if is_video {
+                                video_prod.as_ref().unwrap().len()
+                            } else {
+                                usize::MAX
+                            };
+
+                            let a = if is_audio {
+                                audio_prod.as_ref().unwrap().len()
+                            } else {
+                                usize::MAX
+                            };
+
+                            if matches!(*control.status.lock().unwrap(), Status::Finish)
+                                || !matches!(*control.task.lock().unwrap(), Task::Play)
+                                || v <= DECODE_QUEUE_LEN
+                                || a <= DECODE_QUEUE_LEN
+                            {
+                                break;
+                            }
+
+                            thread::sleep(Duration::from_micros(50)); // just a random value that works
+                        }
                     }
 
-                    thread::sleep(Duration::from_micros(50)); // just a random value that works
-                }
-            }
+                    if is_video {
+                        decoder.video_decoder.as_mut().unwrap().send_eof()?;
+                        Self::video_decode(
+                            decoder.video_decoder.as_mut().unwrap(),
+                            scaler.as_mut().unwrap(),
+                            video_prod.as_mut().unwrap(),
+                        )?;
+                    }
+                    if is_audio {
+                        decoder.audio_decoder.as_mut().unwrap().send_eof()?;
+                        Self::audio_decode(
+                            decoder.audio_decoder.as_mut().unwrap(),
+                            resampler.as_mut().unwrap(),
+                            audio_prod.as_mut().unwrap(),
+                        )?;
+                        Self::audio_flush(
+                            resampler.as_mut().unwrap(),
+                            audio_prod.as_mut().unwrap(),
+                        )?;
+                    }
 
-            if is_video {
-                decoder.video_decoder.as_mut().unwrap().send_eof()?;
-                Self::video_decode(
-                    decoder.video_decoder.as_mut().unwrap(),
-                    scaler.as_mut().unwrap(),
-                    video_prod.as_mut().unwrap(),
-                )?;
-            }
-            if is_audio {
-                decoder.audio_decoder.as_mut().unwrap().send_eof()?;
-                Self::audio_decode(
-                    decoder.audio_decoder.as_mut().unwrap(),
-                    resampler.as_mut().unwrap(),
-                    audio_prod.as_mut().unwrap(),
-                )?;
-                Self::audio_flush(resampler.as_mut().unwrap(), audio_prod.as_mut().unwrap())?;
-            }
+                    *control.status.lock().unwrap() = Status::Finish;
 
-            Ok(())
-        })
+                    Ok(())
+                })
+            },
+            control,
+        )
     }
 
     /// return `Ok(is_video)`
